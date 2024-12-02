9d7edd0ecbae0cc485f33d109edecb4bf41f4c2d
patrickt <patrickt@node0.yuhong-227908.e6897-pg0.utah.cloudlab.us>
2024-12-01 17:19:52 -0700

    add cpu utilization profiling

--- memory_bandwidth/memory_benchmark.sh
+++ memory_bandwidth/memory_benchmark.sh
@@ -14,10 +14,10 @@ DIST_FN=l2
 L_VALUES=(10 20 30 40 50)
 #L_VALUES=(10)
 BW=4
-T=48
+T=16
 LS="10"
 CACHE=0
-CACHE_L=(0 10000000 100000000)
+CACHE_L=(0 1000000 10000000)
 M=100
 R=64
 BUILD_L=100
@@ -40,6 +40,8 @@ K=10
 sudo modprobe amd_uncore
 mkdir -p ${INDEX_PREFIX_PATH}/search
 mkdir -p ${INDEX_PREFIX_PATH}/result
+mkdir amd_results
+mkdir cpu_results
 if [ ! -d "$INDEX_PREFIX_PATH" ]; then
     echo "Directory $INDEX_PREFIX_PATH is not exist. Build it first?"
     exit 1
@@ -68,68 +70,65 @@ for MEM_L in ${MEM_LS[@]}
 do
     for L in ${L_VALUES[@]}
     do
-        SEARCH_LOG=${INDEX_PREFIX_PATH}search/search_SQ${USE_SQ}_K${K}_L${L}_CACHE${CACHE}_BW${BW}_T${T}_MEML${MEM_L}_MEMK${MEM_TOPK}_MEM_USE_FREQ${MEM_USE_FREQ}_PS${USE_PAGE_SEARCH}_USE_RATIO${PS_USE_RATIO}_GP_USE_FREQ{$GP_USE_FREQ}_GP_LOCK_NUMS${GP_LOCK_NUMS}_GP_CUT${GP_CUT}.log
-        echo "Searching... log file: ${SEARCH_LOG}"
-        sync; echo 3 | sudo tee /proc/sys/vm/drop_caches; nohup ${EXE_PATH}/tests/search_disk_index --data_type $DATA_TYPE \
-            --dist_fn $DIST_FN \
-            --index_path_prefix $INDEX_PREFIX_PATH \
-            --query_file $QUERY_FILE \
-            --gt_file $GT_FILE \
-            -K $K \
-            --result_path ${INDEX_PREFIX_PATH}result/result \
-            --num_nodes_to_cache $CACHE \
-            -T $T \
-            -L $L \
-            -W $BW \
-            --mem_L ${MEM_L} \
-            --mem_index_path ${MEM_INDEX_PATH}_index \
-            --use_page_search ${USE_PAGE_SEARCH} \
-            --use_ratio ${PS_USE_RATIO} \
-            --disk_file_path ${DISK_FILE_PATH} \
-            --benchmark 1 \
-            --use_sq ${USE_SQ} > ${SEARCH_LOG} &
-        pid=$!;
-        echo ${pid};
-        log_arr+=( ${SEARCH_LOG} );
-        sleep 60;
-        echo "Beginning Memory Analysis";
-        cd /opt/AMDuProf_5.0-1479/bin;
-        MEMORY_LOG=/users/patrickt/mnt/sdb/starling/memory_bandwidth/amd_results/${L}_${MEM_L}_results.csv;
-        sudo ./AMDuProfPcm -m memory -a -d 10 -o ${MEMORY_LOG};
-        echo "Killing Search";
-        kill -9 ${pid};
-        cd /users/patrickt/mnt/sdb/starling/memory_bandwidth/;
+        for CACHE in ${CACHE_L[@]}
+        do
+            SEARCH_LOG=${INDEX_PREFIX_PATH}search/search_SQ${USE_SQ}_K${K}_L${L}_CACHE${CACHE}_BW${BW}_T${T}_MEML${MEM_L}_MEMK${MEM_TOPK}_MEM_USE_FREQ${MEM_USE_FREQ}_PS${USE_PAGE_SEARCH}_USE_RATIO${PS_USE_RATIO}_GP_USE_FREQ{$GP_USE_FREQ}_GP_LOCK_NUMS${GP_LOCK_NUMS}_GP_CUT${GP_CUT}.log
+            echo "Searching... log file: ${SEARCH_LOG}"
+            sync; echo 3 | sudo tee /proc/sys/vm/drop_caches; nohup ${EXE_PATH}/tests/search_disk_index --data_type $DATA_TYPE \
+                --dist_fn $DIST_FN \
+                --index_path_prefix $INDEX_PREFIX_PATH \
+                --query_file $QUERY_FILE \
+                --gt_file $GT_FILE \
+                -K $K \
+                --result_path ${INDEX_PREFIX_PATH}result/result \
+                --num_nodes_to_cache $CACHE \
+                -T $T \
+                -L $L \
+                -W $BW \
+                --mem_L ${MEM_L} \
+                --mem_index_path ${MEM_INDEX_PATH}_index \
+                --use_page_search ${USE_PAGE_SEARCH} \
+                --use_ratio ${PS_USE_RATIO} \
+                --disk_file_path ${DISK_FILE_PATH} \
+                --benchmark 1 \
+                --use_sq ${USE_SQ} > ${SEARCH_LOG} &
+            pid=$!;
+            echo ${pid};
+            log_arr+=( ${SEARCH_LOG} );
+            # result_path="${INDEX_PREFIX_PATH}result/result";
+            #echo "${SEARCH_LOG}"
+            # check for QPS text in header file, indicating that the program has begun searching
+            # queries
+            while true; do
+                grep -q "QPS" "${SEARCH_LOG}"
+                result=$? 
+
+                if [ $result -eq 0 ]; then
+                    break 
+                elif [ $result -eq 1 ]; then
+                    sleep 60
+                fi
+            done
+            sleep 30;
+            echo "Beginning Memory Analysis";
+            cd /opt/AMDuProf_5.0-1479/bin;
+            MEMORY_LOG=/users/patrickt/mnt/sdb/starling/memory_bandwidth/amd_results/${L}_${MEM_L}_${CACHE}_results.csv;
+            sudo ./AMDuProfPcm -m memory -a -d 10 -o ${MEMORY_LOG};
+            
+            echo "Beginning CPU Utilization Analysis";
+            samples=5;
+            output_file=/users/patrickt/mnt/sdb/starling/memory_bandwidth/cpu_results/${L}_${MEM_L}_${CACHE}_results.csv;
+            for ((i=1; i<=samples; i++)); do
+                timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+                sudo ps -eo pid,pcpu,comm | grep "search_disk" | grep -v "grep" | awk -v ts="$timestamp" '{print ts","$1","$2","$3}' >> "$output_file"
+                sleep 10
+            done
+            
+            echo "Killing Search";
+            kill -9 ${pid};
+            cd /users/patrickt/mnt/sdb/starling/memory_bandwidth/;
+        done
         # while kill -0 $pid 2>/dev/null; do
         # 	sleep 1
     done
 done
-    # done
-    # ;;
-
-# echo "Starting search with $L"
-# cd $SEARCH_PATH
-#     sync; echo 3 | sudo tee /proc/sys/vm/drop_caches; ./search_disk_index \
-#     --data_type $data_type \
-#     --dist_fn l2 \
-#     --index_path_prefix ${INDEX_PREFIX_PATH} \
-#     --query_file $query \
-#     --gt_file $gt_file \
-#     -K $K \
-#     -L ${l_values} \
-#     --result_path $result \
-#     --num_nodes_to_cache $cache \
-#     -T $T \
-#     -W $BW \
-#     --mem_L ${MEM_L} \
-#     --mem_index_path ${MEM_INDEX_PATH}_index \
-#     --use_page_search ${USE_PAGE_SEARCH} \
-#     --use_ratio ${PS_USE_RATIO} \
-#     --disk_file_path ${DISK_FILE_PATH} \
-#     --use_sq ${USE_SQ}
-#     #search_pid=$!
-#     #sleep 60
-# #done
-# #done
-
-
-# # cd /opt/AMDuProf_5.0-1479/bin
