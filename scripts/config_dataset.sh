4e6da748b0c8dc56de0226ed0c085dd35644d0e3
patrickt <patrickt@node0.starling.e6897-pg0.utah.cloudlab.us>
2025-01-25 19:50:17 -0700

    updated dataset parameters

--- scripts/config_dataset.sh
+++ scripts/config_dataset.sh
@@ -6,14 +6,14 @@
 #   BIGANN10M   #
 #################
 dataset_bigann10M() {
-  BASE_PATH=/data/datasets/BIGANN/base.10M.u8bin
-  QUERY_FILE=/data/datasets/BIGANN/query.public.10K.128.u8bin
-  GT_FILE=/data/datasets/BIGANN/bigann-10M-gt.bin 
-  PREFIX=bigann_10m
-  DATA_TYPE=uint8
+  BASE_PATH=/mnt/sdb/SPTAG/datasets/SPACEV1B/vectors.bin/vectors_merged.bin
+  QUERY_FILE=/mnt/sdb/SPTAG/datasets/SPACEV1B/query.bin
+  GT_FILE=/mnt/sdb/DiskANN/build/spacev1b/spacev1b_gt
+  PREFIX=/mnt/sdb/starling/disk_index/spaceV1B
+  DATA_TYPE=int8
   DIST_FN=l2
-  B=0.3
+  B=3.25
   K=10
-  DATA_DIM=128
-  DATA_N=10000000
+  DATA_DIM=100
+  DATA_N=100000000
 }
